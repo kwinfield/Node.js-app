bc9136c9497232bde944fba5392793f5b7bf90fe
Keith Winfield <keith.r.winfield@gmail.com>
2018-09-18 14:25:29 -0400

    full profile

---
+++ example_profile.js
@@ -0,0 +1,15 @@
+var profile = require("./profile.js");
+
+
+var studentProfile = new Profile("chalkers");
+
+//When the JSON body is fully received 
+//the "end" even is triggered and the full body 
+//is given to the handler or callback
+
+studentProfile.on("end", console.dir);
+
+//If a parsing, network or HTTP error occurs and 
+//error object is passed in to the handler or callback
+
+studentProfile.on("error", console.error);
